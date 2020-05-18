a7c835f58b865c7db5337cf76f8b44b6c340284f
Alexander Strutz <alexander.strutz@able-group.de>
2020-05-18 09:54:14 +0200

    Searchable table

--- web/debitorlist.js
+++ web/debitorlist.js
@@ -9,3 +9,24 @@ window.onload = function () {
         return new mdc.dataTable.MDCDataTable(el);
     });
 };
+
+function search() {
+    //Adapted from https://www.w3schools.com/howto/howto_js_filter_table.asp
+    let input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("search");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("debitorList");
+    tr = table.getElementsByTagName("tr");
+
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[1];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
