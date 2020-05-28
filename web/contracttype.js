14e6af3ef70f07054735351e7dbfd0d376755a4e
Alexander Strutz <alexander.strutz@able-group.de>
2020-05-28 09:37:07 +0200

    Some icon vodoo

--- web/contracttype.js
+++ web/contracttype.js
@@ -5,6 +5,8 @@ const contractStatus = {
 }
 
 window.onload = async function () {
+    console.log($('.contentWrapper'));
+
     mdc.linearProgress.MDCLinearProgress.attachTo(document.querySelector('.mdc-linear-progress'));
 
     const dataTables = [].map.call(document.querySelectorAll('.mdc-data-table'), function (el) {
@@ -19,7 +21,29 @@ window.onload = async function () {
 
 async function getContractTypes() {
     //Possible Types: Arbeitnehmerüberlassung, Dienstvertrag, Werkvertrag, Personalvermittlung
-    return [{ title: "Arbeitnehmerüberlassung", exists: true, signed: false }];
+    //TODO get Types from d3 Properties
+    return [
+        {
+            title: "Arbeitnehmerüberlassung",
+            exists: true,
+            signed: false
+        },
+        {
+            title: "Dienstvertrag",
+            exists: false,
+            signed: false
+        },
+        {
+            title: "Werkvertrag",
+            exists: false,
+            signed: false
+        },
+        {
+            title: "Personalvermittlung",
+            exists: true,
+            signed: true
+        }
+    ];
 }
 
 async function getContractStatus() {
@@ -31,27 +55,29 @@ function displayContractStatus(contract) {
         $('h1').text(contract.name);
     } else {
         $('h1').text("Fehler: Kein Name hinterlegt.");
-    }
+    }/*
+    console.log(contract.status);
+    console.log(contractStatus.running);
     switch (contract.status) {
-        case contractStatus.running: $('.contractStatus').css('color', '#2ecc71'); break;
-        case contractStatus.noticed: $('.contractStatus').css('color', '#f1c40f'); break;
-        case contractStatus.terminated: $('.contractStatus').css('color', '#e74c3c'); break;
-        default: $('.contractStatus').hide();
-    }
+        case contractStatus.running: $('#contractStatus').css('color', '#2ecc71'); $('#contractStatus').show(); console.log($('#contractStatus')); console.error("53"); break;
+        case contractStatus.noticed: $('#contractStatus').css('color', '#f1c40f'); console.error("34"); break;
+        case contractStatus.terminated: $('#contractStatus').css('color', '#e74c3c'); console.error("2H"); break;
+        //default: $('.contractStatus').hide(); console.error("AH");
+    }*/
 }
 
 function displayContractTypes(contractTypes) {
-    let tableHtml = '',
+    let tableHtml = '';
     if (contractTypes.length > 0) {
         for (let i in contractTypes) {
             tableHtml += createTableRow(contractTypes[i]);
-            //title, exists, signed
         }
         $('.mdc-data-table__content').html(tableHtml);
         $('.loading').hide();
         $('.contractTypeWrapper').show();
     } else {
-        //TODO: Display that no types are found
+        $('.loading').hide();
+        $('.contractTypeWrapper').html('<p>Es wurden keine Vertragsarten gefunden.</p>');
     }
 }
 
@@ -60,17 +86,22 @@ function createTableRow(contractType) {
     rowHtml += '<tr class="mdc-data-table__row">';
     rowHtml += createTableCell(contractType.title);
     if (contractType.exists) {
-        rowHtml += createTableCell('<span class="material-icons">check_circle</span>');
+        rowHtml += createTableCell('<span class="material-icons existsIcon">check_circle</span>', 'existsCell');
     } else {
-        rowHtml += createTableCell('');
+        rowHtml += createTableCell();
+    }
+    if (contractType.signed) {
+        rowHtml += createTableCell('<span class="material-icons signedIcon">check_circle</span>');
+    } else {
+        rowHtml += createTableCell();
     }
     rowHtml += '</tr>';
     return rowHtml;
 }
 
-function createTableCell(content) {
+function createTableCell(content = "", additionalClass = "") {
     let cellHtml;
-    cellHtml += '<td class="mdc-data-table__cell">';
+    cellHtml += '<td class="mdc-data-table__cell ' + additionalClass + '">';
     cellHtml += content;
     cellHtml += '</td>';
     return cellHtml;
