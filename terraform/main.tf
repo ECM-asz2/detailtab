3282cd1cd958504aea5f5a7a247d859f9dde363d
Alexander Strutz <alexander.strutz@able-group.de>
2021-05-11 10:40:24 +0200

    Rollout 1.1.1

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,10 +11,10 @@ locals {
   # If you add stages use keys which result in the stage appended at the end of the list!
   # If you are uncertain use terraform plan to check the changes terraform would make.
   stages = {
-    "a_prod" = "100"
+    "a_prod" = "102"
     "b_dev"  = "$LATEST"
-    "c_qas" = "100"
-    "d_version" = "100"
+    "c_qas" = "102"
+    "d_version" = "102"
   }
 
   // to avoid unnecessary lambda function deployments the build version env var is only changed if the lambda function code has been changed
